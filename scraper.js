4cb278691c3d4c1c8c2116caa158bedc98ee230c
Michael Laythe <mrlaythe24@aol.com>
2016-04-06 14:27:23 -0700

    Added comments, can check if an array element appears on the twitter homepage & an object keeps track of how many times it does

--- scraper.js
+++ scraper.js
@@ -1,20 +1,30 @@
 var request = require('request'),
     cheerio = require('cheerio'),
     url = 'https://twitter.com/',
-    names = {};
+    messages = {};
 
 request(url, function(err, res, body) {
     if(!err && res.statusCode === 200) {
+        //load the body of the page into Cheerio so we can traverse the DOM
         var $ = cheerio.load(body)
-        $('strong.fullname', 'div.stream-item-header').each(function() {
-            var name = $(this).html();
-            if(names[name] === 1) {
-                names[name] += 1;
-                console.log('repeats more than once', name);
-            } else {
-                names[name] = 1;
+        //looks at the text inside every tweet on Twitter homepage
+        $('p.tweet-text', 'div.js-tweet-text-container').each(function() {
+            var message = $(this).text(),
+                //specify your keyword search by changing elements in array
+                key  = ['Bernie', 'Sophomore All-American', 'Merle Haggard'];
+            //adds number of times the keyword appears on homepage and
+            //makes that the value of the keyword property in the object
+            for(var i = 0; i < key.length; i++) {
+                 if(message.indexOf(key[i]) !== -1) {
+                    if(messages[key[i]] === 1){
+                        messages[key[i]] += 1;
+                    } else {
+                        messages[key[i]] = 1;
+                    }
+                }
             }
         });
-        console.log(names);
+        //logs the results of the keyword search
+        console.log(messages);
     }
 });
